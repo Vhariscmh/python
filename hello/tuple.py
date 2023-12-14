efcf1e2e1447345714d2e3a209c555c8e8bf09fe
vhariscmh <vharisabdul@gmail.com>
2023-12-14 19:23:12 +0530

    tuple

---
+++ hello/tuple.py
@@ -0,0 +1,17 @@
+#creating a tuple  
+rainbow=("v","i","b","g","y","o","r") 
+print(rainbow)  
+colour=("violet","blue","green","yellow","orange","red")  
+print(colour)  
+# Add items in tuples 
+rainbow_colour=rainbow+colour  
+print(rainbow_colour) 
+#length of the tuple  
+c=len(rainbow_colour)  
+print(c)  
+#Access items in tuple  
+print("rainbow[2]:",rainbow[2]) 
+"""rainbow[1:3] means all the items in rainbow tuple
+starting from an index value of 1 up to an index value of 4""" 
+print("rainbow[1:3]",rainbow[1:3])  
+print("rainbow[0:4]",rainbow[0:4]) 
